d5283ea98b3cfcd6abaef3478407b46ada44ff01
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-28 10:34:43 +0000

    updates

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  final VoidCallback showPromptScreen;
+  const HomeScreen({super.key, required this.showPromptScreen});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
