64d137837fecd896ab8c2973dff84687bfc22c18
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-29 18:12:01 +0000

    assets updates

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,50 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF6D1B7B),
+              Color(0xFF000000),
+            ],
+          ),
+
+          // Background image here
+          image: DecorationImage(
+            image: AssetImage(
+              "assets/images/worldmap.png",
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        // Column starts here
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // First Expanded
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+              ),
+            ),
+
+            // Second Expanded
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Text('data'),
+              ),
+            ),
+          ],
+        ),
+        // Column ends here
+      ),
+    );
   }
 }
