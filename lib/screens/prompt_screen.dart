1c6a03a9ccbc0ce040c5f4ec25552739f9413db9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-30 12:53:04 +0000

    prompt updates

--- lib/screens/prompt_screen.dart
+++ lib/screens/prompt_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:talkglobal/utility/language_dropdown.dart';
 
 class PromptScreen extends StatefulWidget {
   final VoidCallback showHomeScreen;
@@ -104,78 +105,7 @@ class _PromptScreenState extends State<PromptScreen> {
               ),
 
               // Dropdown for country selection
-              Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Container(
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFFFCCCC).withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20.0),
-                      color: const Color(0xFFFFFFFF).withOpacity(0.3),
-                      border: Border.all(
-                        color: const Color(0xFFFFCCCC).withOpacity(0.8),
-                        width: 0.1,
-                      ),
-                    ),
-                    // Dropdown for country selection
-                    child: DropdownButton<String>(
-                      value: selectedCountry,
-                      hint: Text(
-                        "Select Language",
-                        style: GoogleFonts.poppins(
-                          fontSize: 16.0,
-                          color: const Color(0xFFFFFFFF),
-                        ),
-                      ),
-                      icon: const Padding(
-                        padding: EdgeInsets.only(left: 8.0),
-                        child: Icon(
-                          Icons.keyboard_arrow_down_rounded,
-                          color: Color(0xFFFFFFFF),
-                        ),
-                      ),
-                      underline: Container(
-                        height: 2,
-                        color: Colors.transparent,
-                      ),
-                      dropdownColor: const Color(0xFF6D1B7B),
-                      items: languageData.map((country) {
-                        return DropdownMenuItem<String>(
-                          value: country['countryLanguage'],
-                          child: Row(
-                            children: [
-                              Image.asset(
-                                country['countryImage']!,
-                                width: 24,
-                                height: 24,
-                                fit: BoxFit.cover,
-                              ),
-                              const SizedBox(width: 8),
-                              Text(
-                                country['countryLanguage']!,
-                                style: GoogleFonts.poppins(
-                                  fontSize: 16.0,
-                                  color: const Color(0xFFFFFFFF),
-                                ),
-                              ),
-                            ],
-                          ),
-                        );
-                      }).toList(),
-                      onChanged: (newValue) {
-                        setState(() {
-                          selectedCountry = newValue;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-              ),
+              const LanguageDropdown(),
             ],
           ),
         ),
