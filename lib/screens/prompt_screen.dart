bf1577ee175fa01fb5ecbf644c13d16b80af8751
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-30 02:45:54 +0000

    prompt updates

--- lib/screens/prompt_screen.dart
+++ lib/screens/prompt_screen.dart
@@ -10,6 +10,20 @@ class PromptScreen extends StatefulWidget {
 }
 
 class _PromptScreenState extends State<PromptScreen> {
+  // List of maps containing country names and their corresponding image paths
+  final List<Map<String, String>> countryData = [
+    {'countryName': 'USA', 'countryImage': 'assets/images/usa.png'},
+    {'countryName': 'Russia', 'countryImage': 'assets/images/russia.png'},
+    {'countryName': 'Italy', 'countryImage': 'assets/images/italy.png'},
+    {'countryName': 'Germany', 'countryImage': 'assets/images/germany.png'},
+    {'countryName': 'France', 'countryImage': 'assets/images/france.png'},
+    {'countryName': 'China', 'countryImage': 'assets/images/china.png'},
+    {'countryName': 'England', 'countryImage': 'assets/images/britain.png'},
+    {'countryName': 'Saudi', 'countryImage': 'assets/images/arabic.png'},
+  ];
+
+  String? selectedCountry;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +40,7 @@ class _PromptScreenState extends State<PromptScreen> {
           ),
         ),
 
-        // Padding around contnets
+        // Padding around contents
         child: Padding(
           padding: const EdgeInsets.only(top: 50.0, left: 16.0, right: 16.0),
 
@@ -53,7 +67,7 @@ class _PromptScreenState extends State<PromptScreen> {
                     ),
                   ),
 
-                  // Conatiner for send icon
+                  // Container for send icon
                   Container(
                     padding: const EdgeInsets.all(8.0),
                     decoration: BoxDecoration(
@@ -87,6 +101,78 @@ class _PromptScreenState extends State<PromptScreen> {
                     ),
                   ),
                 ],
+              ),
+              const SizedBox(height: 20),
+
+              // Dropdown for country selection
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFCCCC).withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: const Color(0xFFFFFFFF).withOpacity(0.3),
+                    border: Border.all(
+                      color: const Color(0xFFFFCCCC).withOpacity(0.8),
+                      width: 0.1,
+                    ),
+                  ),
+                  // Dropdown for country selection
+                  child: DropdownButton<String>(
+                    value: selectedCountry,
+                    hint: Text(
+                      "Select Country",
+                      style: GoogleFonts.poppins(
+                        fontSize: 16.0,
+                        color: const Color(0xFFFFFFFF),
+                      ),
+                    ),
+                    icon: const Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                        color: Color(0xFFFFFFFF),
+                      ),
+                    ),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.transparent,
+                    ),
+                    dropdownColor: const Color(0xFF6D1B7B),
+                    items: countryData.map((country) {
+                      return DropdownMenuItem<String>(
+                        value: country['countryName'],
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              country['countryImage']!,
+                              width: 24,
+                              height: 24,
+                              fit: BoxFit.cover,
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              country['countryName']!,
+                              style: GoogleFonts.poppins(
+                                fontSize: 16.0,
+                                color: const Color(0xFFFFFFFF),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (newValue) {
+                      setState(() {
+                        selectedCountry = newValue;
+                      });
+                    },
+                  ),
+                ),
               ),
             ],
           ),
