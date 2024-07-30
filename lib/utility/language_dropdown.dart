3fbba2c0ad6e166ef070f3561892b509aefc85dc
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-30 09:05:09 +0000

    utility added

---
+++ lib/utility/language_dropdown.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LanguageDropdown extends StatefulWidget {
+  const LanguageDropdown({super.key});
+
+  @override
+  State<LanguageDropdown> createState() => _LanguageDropdownState();
+}
+
+class _LanguageDropdownState extends State<LanguageDropdown> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
