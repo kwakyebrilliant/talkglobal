653c14806fad818f6d9c5866c4aed37e04c3db57
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-31 09:09:11 +0000

    prompt updates

--- lib/screens/prompt_screen.dart
+++ lib/screens/prompt_screen.dart
@@ -254,6 +254,43 @@ class _PromptScreenState extends State<PromptScreen> {
                 ),
               ),
             ),
+
+            // Container for arrow forward in a padding
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+
+              // Container for arrow forward in GestureDetector
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF6D1B7B).withOpacity(0.3),
+                  shape: BoxShape.circle,
+                ),
+
+                // Container for arrow forward
+                child: Container(
+                  height: 50.0,
+                  width: 50.0,
+                  padding: const EdgeInsets.all(2.0),
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFFFFFFF),
+                    shape: BoxShape.circle,
+                  ),
+
+                  // Arrow forward centered
+                  child: Center(
+                    // Arrow forward
+                    child: Transform.rotate(
+                      angle: -1.56,
+                      child: Icon(
+                        Icons.send,
+                        color: const Color(0xFF6D1B7B).withOpacity(0.3),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
