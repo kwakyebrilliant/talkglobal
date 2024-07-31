77199f9bf5d58bf2bbfac6b485f9f4ed2a3373b1
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-31 09:47:38 +0000

    translate container added

--- lib/screens/prompt_screen.dart
+++ lib/screens/prompt_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:talkglobal/utility/language_dropdown.dart';
+import 'package:talkglobal/utility/translate_container.dart';
 
 class PromptScreen extends StatefulWidget {
   final VoidCallback showHomeScreen;
@@ -111,75 +112,7 @@ class _PromptScreenState extends State<PromptScreen> {
             ),
 
             // Container for what to translate in padding
-            Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                padding: const EdgeInsets.all(12.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12.0),
-                  color: const Color(0xFFFFFFFF),
-                  border: Border.all(
-                    color: const Color(0xFF6D1B7B).withOpacity(0.8),
-                    width: 0.2,
-                  ),
-                ),
-
-                // Column for translate contents
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Textformfield for text content to translate
-                    TextFormField(
-                      maxLines: 6,
-                      decoration: InputDecoration(
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        labelStyle: GoogleFonts.poppins(
-                          color: const Color(0xFF000000),
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ),
-
-                    // Bottom container for langauge dropdown and mic icon
-                    Container(
-                      padding: const EdgeInsets.only(top: 12.0),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          top: BorderSide(
-                            color: const Color(0xFF6D1B7B).withOpacity(0.8),
-                            width: 0.2,
-                          ),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          // Words count
-                          Text(
-                            '25/250 words',
-                            style: GoogleFonts.poppins(
-                              fontSize: 12.0,
-                              fontWeight: FontWeight.w300,
-                              color: const Color(0xFF000000),
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.volume_up_outlined,
-                                color: const Color(0xFF6D1B7B).withOpacity(0.8),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            const TranslateContainer(),
 
             Padding(
               padding: const EdgeInsets.only(top: 20.0, left: 10.0),
