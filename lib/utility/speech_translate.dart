f68a1ee90f542ce4fede87f389cf251f02a79c0c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-07-31 18:47:24 +0000

    speech traslate added

---
+++ lib/utility/speech_translate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpeechTranslate extends StatefulWidget {
+  const SpeechTranslate({super.key});
+
+  @override
+  State<SpeechTranslate> createState() => _SpeechTranslateState();
+}
+
+class _SpeechTranslateState extends State<SpeechTranslate> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
