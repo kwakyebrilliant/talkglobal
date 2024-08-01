1c19f6cf5cec5046714cd26c67a879373eb28330
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-08-01 10:30:50 +0000

    updates

--- lib/utility/speech_translate.dart
+++ lib/utility/speech_translate.dart
@@ -11,7 +11,35 @@ class _SpeechTranslateState extends State<SpeechTranslate> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(12.0),
+          decoration: BoxDecoration(
+            color: const Color(0xFF6D1B7B).withOpacity(0.3),
+            shape: BoxShape.circle,
+          ),
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            padding: const EdgeInsets.all(2.0),
+            decoration: const BoxDecoration(
+              color: Color(0xFFFFFFFF),
+              shape: BoxShape.circle,
+            ),
+
+            // Arrow forward centered
+            child: Center(
+              // Arrow forward
+              child: Icon(
+                Icons.mic_rounded,
+                size: 60.0,
+                color: const Color(0xFF6D1B7B).withOpacity(0.3),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
