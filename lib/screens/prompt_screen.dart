5c929dee57b7aef278baa812cb7ec7c5a3ace968
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-08-02 22:44:19 +0000

    updates

--- lib/screens/prompt_screen.dart
+++ lib/screens/prompt_screen.dart
@@ -29,20 +29,6 @@ class _PromptScreenState extends State<PromptScreen> {
     });
   }
 
-  bool _showColumn = false;
-
-  void _showSpeechColumn() {
-    setState(() {
-      _showColumn = true;
-    });
-  }
-
-  void _showTextColumn() {
-    setState(() {
-      _showColumn = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,34 +52,19 @@ class _PromptScreenState extends State<PromptScreen> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  _showColumn
-                      ? Text(
-                          "Voice Translation",
-                          style: GoogleFonts.poppins(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w300,
-                            color: const Color(0xFF000000),
-                          ),
-                        )
-                      : Text(
-                          "Text Translation",
-                          style: GoogleFonts.poppins(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.w300,
-                            color: const Color(0xFF000000),
-                          ),
-                        ),
-                  _showColumn
-                      ? const Icon(
-                          Icons.mic_rounded,
-                          color: Color(0xFF000000),
-                          size: 24.0,
-                        )
-                      : const Icon(
-                          Icons.text_fields,
-                          color: Color(0xFF000000),
-                          size: 24.0,
-                        ),
+                  Text(
+                    "Text Translation",
+                    style: GoogleFonts.poppins(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w300,
+                      color: const Color(0xFF000000),
+                    ),
+                  ),
+                  const Icon(
+                    Icons.text_fields,
+                    color: Color(0xFF000000),
+                    size: 24.0,
+                  ),
                 ],
               ),
             ),
@@ -160,9 +131,7 @@ class _PromptScreenState extends State<PromptScreen> {
                     width: 0.2,
                   ),
                 ),
-                child: _showColumn
-                    ? const SpeechTranslate()
-                    : const TextTranslate(),
+                child: const TextTranslate(),
               ),
             ),
             Padding(
@@ -213,101 +182,31 @@ class _PromptScreenState extends State<PromptScreen> {
                     width: 0.2,
                   ),
                 ),
-                child: _showColumn
-                    ? const SpeechTranslate()
-                    : const TextTranslate(),
+                child: const TextTranslate(),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 50.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 10.0,
-                      horizontal: 50.0,
-                    ),
-                    width: MediaQuery.of(context).size.width / 2 + 50,
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF6D1B7B).withOpacity(0.6),
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            GestureDetector(
-                              onTap: _showTextColumn,
-                              child: _showColumn
-                                  ? const Icon(
-                                      Icons.text_snippet_outlined,
-                                      color: Color(0xFFF0F0F0),
-                                    )
-                                  : const Icon(
-                                      Icons.text_snippet_rounded,
-                                      color: Color(0xFFF0F0F0),
-                                    ),
-                            ),
-                            Text(
-                              'Text',
-                              style: GoogleFonts.poppins(
-                                fontSize: 12.0,
-                                fontWeight: _showColumn
-                                    ? FontWeight.w300
-                                    : FontWeight.w600,
-                                color: const Color(0xFFF0F0F0),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            GestureDetector(
-                              onTap: _showSpeechColumn,
-                              child: _showColumn
-                                  ? const Icon(
-                                      Icons.mic_rounded,
-                                      color: Color(0xFFF0F0F0),
-                                    )
-                                  : const Icon(
-                                      Icons.mic_none_outlined,
-                                      color: Color(0xFFF0F0F0),
-                                    ),
-                            ),
-                            Text(
-                              'Speech',
-                              style: GoogleFonts.poppins(
-                                fontSize: 12.0,
-                                fontWeight: _showColumn
-                                    ? FontWeight.w600
-                                    : FontWeight.w300,
-                                color: const Color(0xFFF0F0F0),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+              // Container for submit button
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: const Color(0xFF6D1B7B).withOpacity(0.8),
+                ),
+
+                // Submit text centered
+                child: Center(
+                  // Submit text here
+                  child: Text(
+                    'Submit',
+                    style: GoogleFonts.inter(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: const Color(0xFFFFFFFF),
                     ),
                   ),
-                  Container(
-                    padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                        color: const Color(0xFF000080),
-                        borderRadius: BorderRadius.circular(12.0)),
-                    child: Transform.rotate(
-                      angle: -0.8,
-                      child: const Icon(
-                        Icons.send,
-                        color: Color(0xFFF0F0F0),
-                        size: 38.0,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
           ],
